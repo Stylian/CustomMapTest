aab5c5b2684c20a03a659bbdc48d50a2e409e71f
styl <stchatzim@gmail.com>
2016-10-07 17:38:08 +0100

    set up

---
+++ src/main/Runner.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Runner {
+
+	public Runner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("it works");
+
+	}
+
+}
