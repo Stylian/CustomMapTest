6a81f6646170ff78a16194d8596d4a7add1c603b
stylian <stchatzim@gmail.com>
2016-10-10 14:18:54 +0100

    added multithreaded test

--- src/test/ForgettingMapTest.java
+++ src/test/ForgettingMapTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -48,4 +48,67 @@ public class ForgettingMapTest {
 		assertEquals(limit, map.size());
 	}
 	
+	@Test
+	public void testMultipleThreads() {
+
+		int limit = 4;
+		// key : city , value : population
+		final ForgettingMap<String, Integer> map = new ForgettingMap<>(limit);
+
+		Thread t = new Thread() {
+			public void run() {
+				map.add("Philadelphia", 1526006);
+				map.add("Houston", 2296224);
+				map.add("San Antonio", 1327407);
+				map.add("Los Angeles", 3792621);
+				map.add("Phoenix", 1445632);
+				map.add("San Diego", 1307402);
+			}
+		};
+		t.start();
+
+		Thread t1 = new Thread() {
+			public void run() {
+				map.find("San Antonio");
+				map.find("Philadelphia");
+				map.find("Philadelphia");
+				map.find("San Antonio");
+				map.find("Houston");
+				map.find("Phoenix");
+			}
+		};
+		t1.start();	
+
+		Thread t2 = new Thread() {
+			public void run() {
+				map.add("New York", 8550875);
+				map.add("Chicago", 299590);
+				map.add("San Antonio", 1327407);
+				map.add("Phoenix", 1445987);
+				map.add("San Diego", 1309001);
+				map.add("Houston", 2296224);
+			}
+		};
+		t2.start();
+
+		Thread t3 = new Thread() {
+			public void run() {
+				map.find("Houston");
+				map.find("Los Angeles");
+				map.find("Los Angeles");
+				map.find("Houston");
+				map.find("Phoenix");
+			}
+		};
+		t3.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(limit, map.size());
+		
+	}
 }
