a5175816c2ab0c063faa18f861a9af84c86dbd14
styl <stchatzim@gmail.com>
2016-10-10 01:02:33 +0100

    added multithreaded test

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -10,78 +10,146 @@ public class Runner {
 		// key : city , value : population
 		final ForgettingMap<String, Integer> map = new ForgettingMap<>(4);
 		
-		System.out.println("start");
+		new Thread( () -> {
+			
+			map.add("Philadelphia", 1526006);
+			map.add("Houston", 2296224);
+			map.add("San Antonio", 1327407);
+			sleep(50);
+			
+			map.add("Los Angeles", 3792621);
+			map.add("Phoenix", 1445632);
+			map.add("San Diego", 1307402);
+			sleep(50);
+			
+			map.add("Houston", 2296224);
+		}).start();
 		
-		map.add("Philadelphia", 1526006);
-		showChain(map);
-		map.add("Houston", 2296224);
-		showChain(map);
-		map.add("San Antonio", 1327407);
-		showChain(map);
-		showChain(map);
-		map.add("Los Angeles", 3792621);
-		showChain(map);
-		map.add("Phoenix", 1445632);
-		showChain(map);
-		map.add("San Diego", 1307402);
-		showChain(map);
+		new Thread( () -> {
+			
+			sleep(20);
+			map.find("San Antonio");
+			map.find("Philadelphia");
+			map.find("Philadelphia");
+			sleep(20);
+			
+			map.find("San Antonio");
+			map.find("Houston");
+			map.find("Phoenix");
+			
+		}).start();
 		
-		map.find("San Antonio");
-		showChain(map);
-		map.find("San Antonio");
-		showChain(map);
-		map.find("Philadelphia");
-		showChain(map);
-		map.find("Phoenix");
-
-		map.add("New York", 8550875);
-		showChain(map);
-		map.add("Chicago", 299590);
-		showChain(map);
-		map.add("Houston", 2296224);
+		new Thread( () -> {
+			
+			sleep(30);
+			map.add("New York", 8550875);
+			map.add("Chicago", 299590);
+			map.add("San Antonio", 1327407);
+			sleep(50);
+			
+			map.add("Los Angeles", 3798748);
+			map.add("Phoenix", 1445987);
+			map.add("San Diego", 1309001);
+			sleep(50);
+			
+			map.add("Houston", 2296224);
+		}).start();		
 		
-		map.find("San Antonio");
-		showChain(map);
-		map.find("Philadelphia");
-		showChain(map);
-		map.find("Phoenix");
-
-		map.add("New York", 8550405);
-		showChain(map);
-		map.add("Chicago", 269598);
-		showChain(map);
+		new Thread( () -> {
+			
+			sleep(20);
+			map.find("Houston");
+			map.find("Los Angeles");
+			map.find("Los Angeles");
+			sleep(20);
+			
+			map.find("San Antonio");
+			map.find("Houston");
+			map.find("Phoenix");
+			
+		}).start();		
 		
-		map.find("Los Angeles");
-		showChain(map);
-		map.find("Phoenix");
-		showChain(map);
-		map.find("San Diego");
-
-		map.find("Philadelphia");
-		showChain(map);
-		map.find("Philadelphia");
-		showChain(map);
-		map.find("Los Angeles");
-		showChain(map);
-		map.find("New York");
-		showChain(map);
-		map.find("Phoenix");
-		showChain(map);
-		map.find("San Diego");
-		showChain(map);
 		
-		map.add("New York", 8550875);
-		showChain(map);
-		map.add("Chicago", 299590);
-		showChain(map);
-		map.add("Houston", 2296224);
-		showChain(map);
-		map.add("Phoenix", 1448798);
-		showChain(map);
-		map.add("San Diego", 1309001);
-		showChain(map);
-		
-		System.out.println("end");
+//		System.out.println("start");
+//		
+//		map.add("Philadelphia", 1526006);
+//		showChain(map);
+//		map.add("Houston", 2296224);
+//		showChain(map);
+//		map.add("San Antonio", 1327407);
+//		showChain(map);
+//		showChain(map);
+//		map.add("Los Angeles", 3792621);
+//		showChain(map);
+//		map.add("Phoenix", 1445632);
+//		showChain(map);
+//		map.add("San Diego", 1307402);
+//		showChain(map);
+//		
+//		map.find("San Antonio");
+//		showChain(map);
+//		map.find("San Antonio");
+//		showChain(map);
+//		map.find("Philadelphia");
+//		showChain(map);
+//		map.find("Phoenix");
+//
+//		map.add("New York", 8550875);
+//		showChain(map);
+//		map.add("Chicago", 299590);
+//		showChain(map);
+//		map.add("Houston", 2296224);
+//		
+//		map.find("San Antonio");
+//		showChain(map);
+//		map.find("Philadelphia");
+//		showChain(map);
+//		map.find("Phoenix");
+//
+//		map.add("New York", 8550405);
+//		showChain(map);
+//		map.add("Chicago", 269598);
+//		showChain(map);
+//		
+//		map.find("Los Angeles");
+//		showChain(map);
+//		map.find("Phoenix");
+//		showChain(map);
+//		map.find("San Diego");
+//
+//		map.find("Philadelphia");
+//		showChain(map);
+//		map.find("Philadelphia");
+//		showChain(map);
+//		map.find("Los Angeles");
+//		showChain(map);
+//		map.find("New York");
+//		showChain(map);
+//		map.find("Phoenix");
+//		showChain(map);
+//		map.find("San Diego");
+//		showChain(map);
+//		
+//		map.add("New York", 8550875);
+//		showChain(map);
+//		map.add("Chicago", 299590);
+//		showChain(map);
+//		map.add("Houston", 2296224);
+//		showChain(map);
+//		map.add("Phoenix", 1448798);
+//		showChain(map);
+//		map.add("San Diego", 1309001);
+//		showChain(map);
+//		
+//		System.out.println("end");
+	}
+	
+	public static void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static <K, V> void showChain(ForgettingMap<String, Integer> map) {
